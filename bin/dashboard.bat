59024a7f05af02336e78a072e2481cd06c68ab80
Thomas Ciancetta <18586@studenti.marconiverona.edu.it>
2022-02-16 08:44:55 +0100

    Add files via upload

---
+++ bin/dashboard.bat
@@ -0,0 +1,19 @@
+@ECHO off
+
+Echo '%date% %time%';'Inizio esecuzione file batch' >> ..\\log\\batch_log.log
+
+py --version >NUL
+if errorlevel 0 goto YesPython
+Echo '%date% %time%';'Python Error: Python not installed' >> ..\\log\\batch_log.log
+goto PostPython
+
+:YesPython
+python.exe dashboard.py
+if errorlevel 0 goto PythonSuccess
+Echo '%date% %time%';'PythonFile:Error' >> ..\\log\\batch_log.log
+goto PostPython
+:PythonSuccess
+Echo '%date% %time%';'PythonFile:OK' >> ..\\log\\batch_log.log
+:PostPython
+
+Echo '%date% %time%';'Fine esecuzione file batch' >> ..\\log\\batch_log.log
